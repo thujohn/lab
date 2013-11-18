dc55965bd3c0109e43a8af8f334713f18c1ff84e
Jonathan Thuau <jonathan.thuau@gmail.com>
2013-11-18 22:25:57 +0100

    Add new examples with dompdf

--- app/routes.php
+++ app/routes.php
@@ -13,9 +13,29 @@
 
 Route::get('pdf/image', function()
 {
-    $html = '<html><body>'
-            . '<p>Laravel is beautiful !</p>'
-            . '<img src="'.asset('Logo-Laravel.png').'" alt="" />'
-            . '</body></html>';
+    $html = View::make('pdf.image');
+    return PDF::load($html, 'A4', 'portrait')->download();
+});
+
+Route::get('pdf/utf8', function()
+{
+    $html = View::make('pdf.utf8');
     return PDF::load($html, 'A4', 'portrait')->show();
+});
+
+Route::get('pdf/css', function()
+{
+    $html = View::make('pdf.css');
+    return PDF::load($html, 'A4', 'portrait')->download();
+});
+
+Route::get('pdf/multi', function()
+{
+	for ($i=1;$i<=2;$i++)
+	{
+		$pdf = new \Thujohn\Pdf\Pdf();
+		$content = $pdf->load(View::make('pdf.image'))->output();
+		File::put(public_path('test'.$i.'.pdf'), $content);
+	}
+	PDF::clear();
 });
---
+++ app/views/pdf/image.blade.php
@@ -0,0 +1,6 @@
+<html>
+<body>
+<p>Laravel is beautiful !</p>
+<img src="{{ asset('assets/img/Logo-Laravel.png') }}" alt="" />
+</body>
+</html>
